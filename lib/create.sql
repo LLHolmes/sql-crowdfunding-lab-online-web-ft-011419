13b95918f55dd04a1e2432c72b0742ba600a3ed0
Lana Holmes <lana.lee.holmes@gmail.com>
2019-02-13 14:39:54 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -26,19 +26,17 @@ category TEXT,
 funding_goal NUMERIC,
 start_date TEXT,
 end_date TEXT
-)
-;
+);
 
 CREATE TABLE users (
 id INTEGER PRIMARY KEY,
 name TEXT,
 age INTEGER
-)
-;
+);
 
 CREATE TABLE pledges (
 id INTEGER PRIMARY KEY,
 amount NUMERIC,
 user_id INTEGER,
 project_id INTEGER
-)
+);
